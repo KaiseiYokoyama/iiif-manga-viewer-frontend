c1815f601bc4ecfe882259cf6d76af449f5dcfca
Unknown <crome110mine@gmail.com>
2019-07-20 15:17:00 +0900

    place contrast range

--- src/js/IIIFMangaViewer.js
+++ src/js/IIIFMangaViewer.js
@@ -477,6 +477,36 @@ async function run() {
                             '</div>';
                         dropdown.appendChild(li);
                     }
+                    {
+                        // contrast
+                        const li = document.createElement('li');
+                        li.innerHTML =
+                            '<div class="input-field">' +
+                            '   <i class="material-icons prefix">brightness_medium</i> ' +
+                            '   <form action="#">' +
+                            '       <label>Brightness</label>' +
+                            '       <p class="range-field">' +
+                            '           <input type="range" value="100" min="0" max="200" />' +
+                            '       </p>' +
+                            '   </form>' +
+                            '</div>';
+                        dropdown.appendChild(li);
+                    }
+                    {
+                        // contrast
+                        const li = document.createElement('li');
+                        li.innerHTML =
+                            '<div class="input-field">' +
+                            '   <i class="material-icons prefix">brightness_medium</i> ' +
+                            '   <form action="#">' +
+                            '       <label>Brightness</label>' +
+                            '       <p class="range-field">' +
+                            '           <input type="range" value="100" min="0" max="200" />' +
+                            '       </p>' +
+                            '   </form>' +
+                            '</div>';
+                        dropdown.appendChild(li);
+                    }
                     navBar.appendChild(dropdown);
                 }
                 navWrapper.appendChild(ulR);
